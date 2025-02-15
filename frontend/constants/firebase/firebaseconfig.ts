8d956d5f9692ce58f85d5bf6f41126bfaa2a069b
andyp4293 <andyp4293@gmail.com>
2025-02-15 13:27:38 -0500

    Added firebase packages

---
+++ frontend/constants/firebase/firebaseconfig.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import auth from "@react-native-firebase/auth";
+import database from "@react-native-firebase/database";
+import storage from "@react-native-firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_URL,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { auth, database, storage };
